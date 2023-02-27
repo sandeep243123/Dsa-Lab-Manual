acfd3f167826ce0c331f36fa48de9abe49d9bacc
sandeep243123 <108993768+sandeep243123@users.noreply.github.com>
2023-02-28 01:23:48 +0530

    Add files via upload

---
+++ LinkedList/Para.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class node
+{
+    int data;
+    node next;
+
+}
+class List
+{
+
+    public node insert(node head,int data[])
+    {
+        node t=null;
+        for(int i=0;i<data.length;i++) {
+            node temp = new node();
+            temp.data=data[i];
+            if (head == null) {
+                head = temp;
+                t = temp;
+            } else {
+                t.next = temp;
+                t=temp;
+            }
+        }
+        return head;
+    }
+    public void show(node head)
+    {
+        if(head==null)
+            return ;
+        System.out.print(head.data+" ");
+        show(head.next);
+    }
+
+}
+public class Para {
+    public static void main(String []args) {
+    node head=null;
+        int arr[]={1,4,2,6,3,8};
+        List l=new List();
+        head=l.insert(head,arr);
+        l.show(head);
+    }
+}
