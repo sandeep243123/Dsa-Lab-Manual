0951d547760451c1e4e87aac90005e8b15afcc04
sandeep243123 <108993768+sandeep243123@users.noreply.github.com>
2023-03-06 22:55:45 +0530

    Add files via upload

---
+++ LinkedList/Program6.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	struct node *link;
+	int data;
+};
+struct node *head=NULL,*temp=NULL;
+void insert(int d)
+{
+	struct node *t=(struct node*)malloc(sizeof(struct node));
+	t->data=d;
+	t->link=NULL;
+	if(head==NULL)
+	{
+		head=t;
+		temp=head;
+	}else{
+		temp->link=t;
+		temp=temp->link;
+	}
+}
+struct node *prev=NULL,*next=NULL;
+void rev()
+{
+
+	if(head)
+	{
+		next=head->link;
+		head->link=prev;
+		prev=head;
+		head=next;
+		rev();	
+	}
+	head=prev;
+}
+void show()
+{
+	temp=head;
+	while(temp){
+		printf("%d ",temp->data);
+		temp=temp->link;
+	}
+	printf("\n");
+}
+void main()
+{
+	int arr[]={3,2,5,4,8,6,9};
+	int i=0;
+	for(i=0;i<7;i++){
+		insert(arr[i]);
+	}
+	show();
+	rev();
+	show();
+	
+}
